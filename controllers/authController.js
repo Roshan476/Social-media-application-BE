da7278c78610ae25c84de317efe0138a6f367a2e
Roshan476 <roshanpoudel864@gmail.com>
2024-12-24 16:54:14 +0545

    backend inital part setup and some files and folders are created

---
+++ controllers/authController.js
@@ -0,0 +1,37 @@
+import Users from "../models/userModel.js"
+import { hashString } from "../utils.js";
+import { sendVerificationEmail } from "../utils/sendEmail.js";
+
+export const register = async (req,res, next)=>{
+    const {firstName,lastName,email,password}=req.body;
+
+      //validate fileds
+  if (!(firstName || lastName || email || password)) {
+    next("Provide Required Fields!");
+    return;
+  }
+
+  try {
+    const userExist = await Users.findOne({ email });
+
+    if (userExist) {
+      next("Email Address already exists");
+      return;
+    }
+
+    const hashedPassword =await hashString(password);
+
+    const user = await Users.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    //send email verification to user
+    sendVerificationEmail(user, res);
+}catch(error){
+    console.log(error);
+    res.status(404).json({message:error.message });
+}
+}
